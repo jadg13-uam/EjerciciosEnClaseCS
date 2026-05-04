6eba9da37d523816a254dd8efc8c17cca4c1910b
Duran <jose.duran@uamv.edu.ni>
2026-05-04 12:16:41 -0600

    Ejericio While1

---
+++ While1/Program.cs
@@ -0,0 +1,28 @@
+﻿//leer un numero entero y mostrar el cuadrado de sus antecesores
+
+using System;
+
+int numero;
+
+while (true)
+{
+    Console.Write("Ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine(), out numero))
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada no valida. Por favor, ingrese un numero entero.");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine("Mostrando el cuadrado de los antecesores de " + numero + ":");
+int cont = 1;
+while(cont < numero)
+{
+    Console.WriteLine("El cuadrado de " + cont + " es: " + Math.Pow(cont, 2));
+    cont++;
+}
